4d8cb0c7f695feb77c53de4906879dbb9179dbe2
dikshya19 <dikshyapokhrel886@gmail.com>
2022-02-08 18:28:08 +0545

    react project

---
+++ data.js
@@ -0,0 +1,30 @@
+const data ={
+    products:[
+        {
+            _id:'1',name:'Brush',category:'Shirts',image:'/images/p1.jpeg',price:120,brand:'Nike',rating:4.5,numReviews:10,  countInStock: 20,description:'high quality product',
+        },
+        {
+            _id:'2',name:'Oil',category:'Shirts',image:'/images/p2.jpeg',price:120,brand:'Nike',rating:4.5,numReviews:10,  countInStock: 20,description:'high quality product',
+        },
+        {
+            _id:'3',name:'Soap',category:'Shirts',image:'/images/p3.jpeg',price:120,brand:'Nike',rating:4.5,numReviews:10,  countInStock: 20,description:'high quality product',
+        },
+        {
+            _id:'4',name:'Watch',category:'Shirts',image:'/images/p1.jpeg',price:120,brand:'Nike',rating:4.5,numReviews:10,  countInStock: 20,description:'high quality product',
+        },
+        {
+            _id:'5',name:'Watch',category:'Shirts',image:'/images/p8.jpeg',price:120,brand:'Nike',rating:4.5,numReviews:10, countInStock: 20,description:'high quality product',
+        },
+        {
+            _id:'6',name:'Watch',category:'Shirts',image:'/images/p3.jpeg',price:120,brand:'Nike',rating:4.5,numReviews:10, countInStock: 20,description:'high quality product',
+        },
+        {
+            _id:'7',name:'Shoe',category:'Shirts',image:'/images/p7.jpeg',price:120,brand:'Nike',rating:4.5,numReviews:10, countInStock: 20,description:'high quality product',
+        },
+        {
+            _id:'8',name:'Shoe',category:'Shirts',image:'/images/p8.jpeg',price:120,brand:'Nike',rating:4.5,numReviews:10, countInStock: 20,description:'high quality product',
+        },
+
+    ]
+}
+export default data;
